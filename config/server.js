ef8de0038acc2b56fb2a0e1d7f6682325a47e17d
isthatsahil <sahil.verma997@gmail.com>
2021-06-06 15:53:12 +0530

    Release v2.0.0 CMS

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '78212f0fa5f95406c621a3097003852a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '84683643-3599-427c-b15f-f44206c2d00d'
+};
